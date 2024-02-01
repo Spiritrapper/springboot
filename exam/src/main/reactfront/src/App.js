dbef3e960d902465e37b607e267ef631cce9f24c
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2024-02-01 20:04:56 +0900

    숫자기록 하기

--- exam/src/main/reactfront/src/App.js
+++ exam/src/main/reactfront/src/App.js
@@ -5,20 +5,27 @@ import { useState } from 'react';
 function App() {
 
   
-  let [num, setNum] = useState(1);
+  const [num, setNum] = useState(0);
 
-  //setNum(2); 직접쓰면 안됨 계속 랜더링되기때문에
+  const [numList, setNumList] = useState([]);
 
-  // setTimeout(() =>{setNum(num = num +1)},1000);
+  function numRecoding () {
+    setNumList([...numList, num])
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-       <div className='number'>{ num }</div>
-       <div style={{backgroundColor:"blue"}}>{ num }</div>
-       {/*<button onClick={()=>{setNum (num= num+1)}}>버튼</button>*/ }
-
-      </header>
+      <div>현재 숫자 : {num}</div>
+       <button onClick={() => {setNum(num + 1)}}>숫자 증가</button>
+       <button onClick={() => {setNum(num - 1)}}>숫자 감소</button>
+       <button onClick={(numRecoding)}>숫자 기록</button>
+       <h1>지정된 숫자</h1>
+       <ul>
+        {/*numList에 리스트로부터 map을 사용해 num해당되는 숫자를 꺼낸다. setNumList를 통해 새로운num이 리스트에 추가된다.*/}
+        {numList.map((num)=>(
+            <li>{num}</li>
+        ))}
+       </ul>
     </div>
   );
 }
